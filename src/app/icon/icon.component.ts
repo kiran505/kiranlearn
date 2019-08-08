f8d106baf94aadc738634741b67fce076682b0ff
kiran bandikatla <bandikatlakiran@gmail.com>
2019-08-08 18:02:30 +0530

    worked on assignment

---
+++ src/app/icon/icon.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  
+
+})
+export class IconComponent implements OnInit {
+
+  @Input() isicon: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+onclick(){
+  this.isicon= !this.isicon;
+}
+}
