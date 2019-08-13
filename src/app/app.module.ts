47efbf4c0fa851a99d527e0d85a5b72862c24a22
kiran bandikatla <bandikatlakiran@gmail.com>
2019-08-13 19:36:42 +0530

    working on reactives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import { AppComponent } from './app.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
-import { FormsModule } from'@angular/forms';
+import { FormsModule, ReactiveFormsModule } from'@angular/forms';
 import { CourseFormComponent } from './course-form/course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 @NgModule({
   declarations: [
     AppComponent,
     InputFormatDirective,
     ZippyComponent,
     ContactFormComponent,
-    CourseFormComponent
+    CourseFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
